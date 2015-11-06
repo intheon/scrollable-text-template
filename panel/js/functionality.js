0fd27010f2aeaa75b24f8a7505a6416ba935cf22
Ben S <allobon@gmail.com>
2015-11-06 14:04:10 +0000

    wow

--- panel/js/functionality.js
+++ panel/js/functionality.js
@@ -1,79 +1,179 @@
 $(function() {
 
-	// change the direction only
-
 	// possible values are horizontal or vertical
 	var direction = "vertical";
 
-/* rest of code */
-	if (direction == "horizontal")
-	{
-		$(".prev").html("&larr;");
-		$(".next").html("&rarr;");
-	}
-	else if (direction == "vertical")
-	{
-		$(".prev").html("&uarr;");
-		$(".next").html("&darr;");
-	}
-
-	$("#scrollable").dragend({
-		"direction": direction,
-		afterInitialize: function() {
-			var first = this.pages[0],
-              	last = this.pages[this.pages.length - 1];
-
-			for (i = 0; i <= this.pages.length - 1; i++) 
-				{
-					$(".pips").append("<div class='pip' data-page='"+(i + 1)+"'>&#9679;</div>");
-				}
-
-			$(".pip:first-child").addClass("active");
-
-			$(".pip").click(function() {
-				var p = $(event.target).data("page");
-
-        		$("#scrollable").dragend({
-          			scrollToPage: p
-       			});
-
-       			$(".pip").removeClass("active");
-
-       			$(event.target).addClass("active");
-   			 });
 
 
-			$(".prev").click(function() {
-        		$("#scrollable").dragend("right");
 
-        		var c = $(".active").data("page");
-        			c -= 1;
 
-        		var l = 0;
+  // leave as is
+	if (direction == "horizontal") useDragEnd(direction);
 
-        		if (c > l)
-        		{
-        			$(".pip").removeClass("active");
-        			$('.pip[data-page="'+c+'"]').addClass("active");
-        		}
-        		else return false;
+  var userAgent = /iPad|iPhone|iPod/.test(navigator.platform);
 
-    		});
+  if (userAgent)
+  {
+    if (direction == "vertical") useDragEnd(direction);
+  }
+  else
+  {
+    if (direction == "vertical") useScrollify(direction);
+  }
 
-    		$(".next").click(function() {
-        		$("#scrollable").dragend("left");
-        		var c = $(".active").data("page");
-        			c += 1;
+});
 
-        		var l = $(".pip").length;
 
-        		if (c <= l)
-        		{
-        			$(".pip").removeClass("active");
-        			$('.pip[data-page="'+c+'"]').addClass("active");
-        		}
-        		else return false;
-   			 });
+
+function useScrollify(direction)
+{
+
+  $(".panel").addClass("scroll");
+
+    // add some pips
+
+    var nPans = $(".panel").length;
+
+    for (i = 0; i < nPans; i++)
+    {
+      $(".pips").append("<div class='pip' data-page='"+(i + 1)+"'>&#9679;</div>");
+    }
+
+    $(".pip:first-child").addClass("active");
+
+    $(".pip").click(function() {
+
+        var p = $(event.target).data("page");
+
+        $.scrollify.move(+(p - 1));
+
+        /*
+
+        $(".pip").removeClass("active");
+
+        $(event.target).addClass("active");
+
+        */
+
+    });
+
+
+    // create 
+  setTimeout(function(){
+      // allow snapping scrolling
+
+
+      $.scrollify({
+        section:    ".scroll",
+        sectionName:  "name",
+        easing:     "easeOutExpo",
+        scrollSpeed:  1000,
+        offset :    0,
+        scrollbars:   false,
+        before:     function(event){
+          // event is just the number of the panel
+          var integer = + (event + 1);
+
+          $(".pip").removeClass("active");
+
+          $('.pip[data-page="'+integer+'"]').addClass("active");
+        }
+      });
+
+  },100)
+
+    $(".prev").html("&#9650;");
+    $(".next").html("&#9660;");
+
+    $(".prev").click(function(event){ 
+
+      $.scrollify.previous();
+    })
+    $(".next").click(function(event){
+
+      $.scrollify.next();
+    })
+
+}
+
+function animatePips(index)
+{
+
+}
+
+
+function useDragEnd(direction)
+{
+  if (direction == "horizontal")
+  {
+    $(".prev").html("&larr;");
+    $(".next").html("&rarr;");
+  }
+  else
+  {
+    $(".prev").html("&#9660;");
+    $(".next").html("&#9650;");
+  }
+
+
+    
+  $("#scrollable").dragend({
+    "direction": direction,
+    afterInitialize: function() {
+      var first = this.pages[0],
+         last = this.pages[this.pages.length - 1];
+
+      for (i = 0; i <= this.pages.length - 1; i++) 
+        {
+          $(".pips").append("<div class='pip' data-page='"+(i + 1)+"'>&#9679;</div>");
+        }
+
+      $(".pip:first-child").addClass("active");
+
+      $(".pip").click(function() {
+        var p = $(event.target).data("page");
+
+            $("#scrollable").dragend({
+                scrollToPage: p
+            });
+
+            $(".pip").removeClass("active");
+
+            $(event.target).addClass("active");
+         });
+
+
+      $(".prev").click(function() {
+            $("#scrollable").dragend("right");
+
+            var c = $(".active").data("page");
+              c -= 1;
+
+            var l = 0;
+
+            if (c > l)
+            {
+              $(".pip").removeClass("active");
+              $('.pip[data-page="'+c+'"]').addClass("active");
+            }
+            else return false;
+
+        });
+
+        $(".next").click(function() {
+            $("#scrollable").dragend("left");
+            var c = $(".active").data("page");
+              c += 1;
+
+            var l = $(".pip").length;
+
+            if (c <= l)
+            {
+              $(".pip").removeClass("active");
+              $('.pip[data-page="'+c+'"]').addClass("active");
+            }
+            else return false;
+         });
 
         },onDragEnd: function() {
 
@@ -84,10 +184,7 @@ $(function() {
 
           $(".pip").eq(this.page).addClass("active");
         }
-	});
+  });
 
 
-
-
-});
-
+}
